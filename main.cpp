df5064eecc471db5e2222a6f28649aa042ffbda2
KomarnitskyKonstantin <komarnitsky.kostya13@mail.ru>
2025-04-07 23:17:54 +0200

    Add: для лабы

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 
 int main()
 {
-	std::cout << "Hello, 1 FIIT and 1 MO!" << std::endl;
+
+	short a = 5;
+	short b = 7;
+	short c = a+b;
+	std::cout<<c<<std::endl; 
+
+
+	std::cout << "char, 1 byte, -128 ... 127;" 
+	<< " short, 2 bytes, -32768 ... 32767;" << 
+	" int, 4 bytes, -2^31 ... 2^31-1;" << 
+	" long long, 8 bytes, -2^63 ... 2^63-1;"<<
+	" ursigned int, 4 bytes, 0 ... 2^32-1;"<<
+	" float 4 bytes, 6-7 a significant symbol;"<<
+	" double 8 bytes, 15-16 a significant symbol;"<<
+	" boll 1 bytes, true/false"<<
+	 std::endl;
 	return 0;
 }
